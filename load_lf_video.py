874353bd63661905ec57da03b6e6c9f2b4ad3d32
Dashpole <dashpole@debian.dashpole>
2015-12-03 14:26:30 -0500

    chagned format of loaded file to individual 2d lists for y, Cb, Cr

--- load_lf_video.py
+++ load_lf_video.py
@@ -4,18 +4,31 @@ from zipfile import ZipFile
 import sys
 from StringIO import StringIO
 
-# USAGE: load_lf_video <video_file_name>.zip
+#Structure:   LFV[File]['Y', 'Cb', or 'Cr'][x][y]
 
-archive = ZipFile(sys.argv[1], 'r')
-for entry in archive.infolist():
-	filename = entry.filename
-	if filename[-4:] == '.png':  #only open png
-		print filename
-		image_data =  archive.read(filename)
-		fh = StringIO(image_data)
-		image = Image.open(fh)
-		ycbr = image.convert('YCbCr')
-		ycbr_array = numpy.ndarray((image.size[1], image.size[0], 3), 'u1', ycbr.tostring())
-		print ycbr_array
-		# TODO: DO THINGS WITH THE ycbr array
-		exit(0)  #Exit after the first right now so we dont print out a ton of stuff
+def load(zipfile):
+	LFV = {}
+	archive = ZipFile(zipfile, 'r')
+	for entry in archive.infolist():
+		filename = entry.filename
+		if filename[-4:] == '.png':  #only open png
+			image_data =  archive.read(filename)
+			fh = StringIO(image_data)
+			image = Image.open(fh)
+			ycbr = image.convert('YCbCr')
+			ycbr_array = numpy.ndarray((image.size[1], image.size[0], 3), 'u1', ycbr.tostring())
+			y = [[x[0] for x in ycbr_row] for ycbr_row in ycbr_array]
+			Cb = [[x[1] for x in ycbr_row] for ycbr_row in ycbr_array]
+			Cr = [[x[2] for x in ycbr_row] for ycbr_row in ycbr_array]
+
+			LFV[filename] = {}
+			LFV[filename]['Y'] = y
+			LFV[filename]['Cb'] = Cb
+			LFV[filename]['Cr'] = Cr
+
+	return LFV
+
+
+
+# USAGE: load_lf_video <video_file_name>.zip
+load(sys.argv[1])
