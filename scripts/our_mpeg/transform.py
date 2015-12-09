c49b547e59a1711a56f0c6c1822274f455691a1d
flamerokz <flamerokz@gmail.com>
2015-12-09 04:43:20 -0500

    initial notebook demo with dct and idct; it appears there is some error that noticeably affects the image reconstruction

---
+++ scripts/our_mpeg/transform.py
@@ -0,0 +1,28 @@
+import numpy as np
+from scipy.fftpack import dct, idct
+
+def transform(array):
+    """
+    Args: 
+        list(list(numpy.ndarray)): 2D list of uint8 numpy arrays
+    Returns:
+        list(list(numpy.ndarry)): the dct coefficients as float32 of each numpy array in the 2D list
+    """
+    return [
+        [dct(array[x][y].astype(np.float32), norm='ortho')
+         for y in range(0, len(array[x]))]
+         for x in range(0, len(array))
+    ]
+
+def inverse_transform(array):
+    """
+    Args:
+        list(list(numpy.ndarray)): 2D list of numpy array dct coefficients as float32 of each numpy array
+    Returns:
+        list(list(numpy.ndarry)): the idct casted to uint8 of each numpy array in the 2D list
+    """
+    return [
+        [idct(array[x][y], norm='ortho').astype(np.uint8)
+         for y in range(0, len(array[x]))]
+         for x in range(0, len(array))
+    ]
